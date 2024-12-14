a59ded3e30f4ff17ab3fd9107003941c9c1c20e7
namlsoft <qamar.naml@gmail.com>
2024-12-14 22:58:18 +0530

    test changes

--- prediction_helper.py
+++ prediction_helper.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import joblib
 
+
 model_young = joblib.load("artifacts/model_young.joblib")
 model_rest = joblib.load("artifacts/model_rest.joblib")
 scaler_young = joblib.load("artifacts/scaler_young.joblib")
